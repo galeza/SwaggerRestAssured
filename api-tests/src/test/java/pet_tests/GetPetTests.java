c62fc942c19e8c43a1964c9ae659ca279fe6bdfc
galeza <galeza.agnieszka@gmail.com>
2020-03-30 22:37:12 +0200

    Refactor get pet test.

--- api-tests/src/test/java/pet_tests/GetPetTests.java
+++ api-tests/src/test/java/pet_tests/GetPetTests.java
@@ -1,19 +1,45 @@
 package pet_tests;
+import base.BaseTest;
 import fr.galeza.example.swagger.client.model.Pet;
 import org.junit.jupiter.api.Test;
 import requests.GetPetApiRequest;
+import requests.PostPetApiRequest;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static util.Constants.GLOBAL_MESSAGE;
 
 
-public class GetPetTests {
+public class GetPetTests extends BaseTest {
+
+        private String category = "cat";
+
     @Test
     public void getPetTest() {
-        Pet pet = new GetPetApiRequest()
-                .pet("123")
+        Pet petToBeCreated = getTestPet(animal_name, Pet.StatusEnum.AVAILABLE, category, photoUrl);
+
+        Pet createdPet = new PostPetApiRequest()
+                .pet(petToBeCreated)
                 .sendRequest()
                 .assertRequestSuccess()
                 .getResponseModel();
 
-        assertThat(pet.getId()).isEqualTo(123L);
+        assertThat(createdPet.getId()).isEqualTo(petToBeCreated.getId());
+
+        Pet pet = new GetPetApiRequest()
+                .pet(String.valueOf(createdPet.getId()))
+                .sendRequest()
+                .assertRequestSuccess()
+                .getResponseModel();
+
+        assertAll(GLOBAL_MESSAGE,
+                () -> assertThat(createdPet.getId()).isEqualTo(createdPet.getId()),
+                () -> assertThat(createdPet.getName()).isEqualTo(createdPet.getName()),
+                () -> assertThat(createdPet.getPhotoUrls()).isEqualTo(createdPet.getPhotoUrls()),
+                () -> assertThat(createdPet.getTags()).isEqualTo(createdPet.getTags()),
+                () -> assertThat(createdPet.getStatus()).isEqualTo(Pet.StatusEnum.AVAILABLE),
+                () -> assertThat(createdPet.getCategory().getName()).isEqualTo(category)
+        );
+
     }
 }
